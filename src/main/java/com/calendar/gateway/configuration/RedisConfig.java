4d06815d22a846054c40848aeff232a569b4ae6b
theobanette <theobanette@hotmail.fr>
2025-12-29 11:32:03 +0100

    ajout config keepalive qui envoie des packets vides pour conserver la connexion avec la base REDIS

--- src/main/java/com/calendar/gateway/configuration/RedisConfig.java
+++ src/main/java/com/calendar/gateway/configuration/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.calendar.gateway.configuration;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
+import org.springframework.boot.data.redis.autoconfigure.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
@@ -7,6 +10,8 @@ import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -27,5 +32,22 @@ public class RedisConfig {
                 .build();
 
         return new ReactiveRedisTemplate<>(factory, serializationContext);
+    }
+
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer lettuceCustomizer() {
+        return builder -> {
+            SocketOptions socketOptions = SocketOptions.builder()
+                    .keepAlive(true)
+                    .connectTimeout(Duration.ofSeconds(5))
+                    .build();
+
+            ClientOptions clientOptions = ClientOptions.builder()
+                    .socketOptions(socketOptions)
+                    .disconnectedBehavior(ClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
+                    .build();
+
+            builder.clientOptions(clientOptions);
+        };
     }
 }
